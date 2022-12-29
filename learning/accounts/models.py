a9e9dccd1090f0170719c646e4d0c3105b04adf1
devpola <sonamoo9771@gmail.com>
2022-12-29 13:17:38 +0900

    Add detail and update view to accounts app

--- learning/accounts/models.py
+++ learning/accounts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.conf import settings
+from django.shortcuts import resolve_url
 
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    address = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=6)
+    address = models.CharField(max_length=100, blank=True)
+    zipcode = models.CharField(max_length=6, blank=True)
+
+    def get_absolute_url(self):
+        return resolve_url('accounts:profile')
--- learning/accounts/urls.py
+++ learning/accounts/urls.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
+from . import views
 
+
+app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='accounts/login_form.html')),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/update/', views.profile_edit, name='profile_update'),
 ]
